9bbabb197f1d5af26dd206e168e269e29d77b344
vladislav_vostok <vladislav.sage@yandex.ru>
2022-11-20 23:50:52 +0300

    lesson7 and start lesson8

---
+++ lesson_8/collections_8_1_vector/target/debug/deps/collections_8_1_vector.d
@@ -0,0 +1,5 @@
+E:\programming\RustProjects\rust_book_conspect\lesson_8\collections_8_1_vector\target\debug\deps\collections_8_1_vector.exe: src\main.rs
+
+E:\programming\RustProjects\rust_book_conspect\lesson_8\collections_8_1_vector\target\debug\deps\collections_8_1_vector.d: src\main.rs
+
+src\main.rs:
